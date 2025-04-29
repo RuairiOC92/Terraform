d401636edb4a07ec9d457fb10e4f3b90a7d3d015
RuairiOC92 <ruairioconnell92@gmail.com>
2025-04-29 17:49:03 +0100

    creating new instance

--- main.tf
+++ main.tf
@@ -1,4 +1,17 @@
-# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  access_key = "<YOUR_ACCESSKEY>"
+  secret_key = "<YOUR_SECRETKEY>"
+  region = "<REGION>"
+}
+
+resource "aws_instance" "web" {
+  ami = "<AMI>"
+  instance_type = "t3.micro"
+
+  subnet_id = "<SUBNET>"
+  vpc_security_group_ids = ["<SECURITY_GROUP>"]
+
+  tags = {
+  "Identity" = "<IDENTITY>"
+  }
 }
