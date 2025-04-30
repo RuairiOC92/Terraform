01f4cfd56e394c29ae8b22bfb9e3b906cc26d39d
RuairiOC92 <ruairioconnell92@gmail.com>
2025-04-30 15:48:09 +0100

    formatting code

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 provider "aws" {
   access_key = "<YOUR_ACCESSKEY>"
   secret_key = "<YOUR_SECRETKEY>"
-  region = "<REGION>"
+  region     = "<REGION>"
 }
 
 resource "aws_instance" "web" {
-  ami = "<AMI>"
+  ami           = "<AMI>"
   instance_type = "t3.micro"
 
-  subnet_id = "<SUBNET>"
+  subnet_id              = "<SUBNET>"
   vpc_security_group_ids = ["<SECURITY_GROUP>"]
 
   tags = {
-  "Identity" = "<IDENTITY>"
+    "Identity" = "<IDENTITY>"
   }
 }
