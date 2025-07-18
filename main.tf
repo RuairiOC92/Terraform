ed24f9c5b6c4c641a7952c2968c0269106775e62
RuairiOC92 <ruairioconnell92@gmail.com>
2025-07-18 16:15:21 +0100

    Clean up embedded repo

--- main.tf
+++ main.tf
@@ -2,32 +2,3 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    gateway_id     = aws_internet_gateway.internet_gateway.id
-    #nat_gateway_id = aws_nat_gateway.nat_gateway.id
-  }
-  tags = {
-    Name      = "demo_public_rtb"
-    Terraform = "true"
-  }
-}
-
-resource "aws_s3_bucket" "my-new-S3-bucket" {   
-  bucket = "my-new-tf-test-bucket-bryan"
-
-  tags = {     
-    Name = "My S3 Bucket"     
-    Purpose = "Intro to Resource Blocks Lab"   
-  } 
-}
-
-resource "aws_s3_bucket_ownership_controls" "my_new_bucket_acl" {   
-  bucket = aws_s3_bucket.my-new-S3-bucket.id  
-  rule {     
-    object_ownership = "BucketOwnerPreferred"   
-  }
-}
