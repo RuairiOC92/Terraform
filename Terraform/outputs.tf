30c87a707c7ebab2239e6c88f2e8ee676def700c
RuairiOC92 <ruairioconnell92@gmail.com>
2025-07-29 14:26:35 +0100

    pushing aws instances

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -2,68 +2,68 @@
 ## Latest AWS availability zones ##
 
 output "availability_zones" {
-    value = data.aws_availability_zones.available.names
-    description = "List of all availability zones"
+  value       = data.aws_availability_zones.available.names
+  description = "List of all availability zones"
 }
 
 
 ## Latest Ubuntu Image ##
 
- output "aws_ami_id" {
-        value = data.aws_ami.ubuntu.id
-        description = "Latest Ubuntu image"
-    }
+output "aws_ami_id" {
+  value       = data.aws_ami.ubuntu.id
+  description = "Latest Ubuntu image"
+}
 
 
 ## Current AWS Account ID ##
 
-    output "account_id" {
-        value = data.aws_caller_identity.current.account_id
-        description = "This is the current AWS account ID"    
-        }
+output "account_id" {
+  value       = data.aws_caller_identity.current.account_id
+  description = "This is the current AWS account ID"
+}
 
 
 ## Creating s3 Bucket ##
 
 output "aws_s3_name" {
-    value = aws_s3_bucket.backup.id
-    description = "The name of the s3 bucket"
+  value       = aws_s3_bucket.backup.id
+  description = "The name of the s3 bucket"
 }
 
 output "aws_s3_arn" {
-    value = aws_s3_bucket.backup.arn
-    description = "The name of the arn created"
+  value       = aws_s3_bucket.backup.arn
+  description = "The name of the arn created"
 }
 
 ## Create CIDR Block ##
 
 output "vpc_id" {
-    value = aws_vpc.main.id
-    description = "ID of VPC created"
+  value       = aws_vpc.main.id
+  description = "ID of VPC created"
 }
 
 ## Create Subnet ##
 
 output "public_subnet" {
-    value = aws_subnet.public_subnet.id
-    description = "ID of Public Subnet created"
+  value       = aws_subnet.public_subnet.id
+  description = "ID of Public Subnet created"
 }
 
 output "private_subnet" {
-    value = aws_subnet.private_subnet.id 
-    description = "ID of Private Subnet created"
+  value       = aws_subnet.private_subnet.id
+  description = "ID of Private Subnet created"
 }
 
 ## Create 5 s3 buckets with count ##
 
-output "bucket_ids"{
-    value = aws_s3_bucket.my-super-awesome-test-bucket[*].id
-    description = "List of all S3 bucket IDs created"
+output "bucket_ids" {
+  value       = aws_s3_bucket.my-super-awesome-test-bucket[*].id
+  description = "List of all S3 bucket IDs created"
 }
 
-output "bucket_arns"{
-    value = aws_s3_bucket.my-super-awesome-test-bucket[*].arn
-    description = "List of all S3 bucket arns created"
+output "bucket_arns" {
+  value       = aws_s3_bucket.my-super-awesome-test-bucket[*].arn
+  description = "List of all S3 bucket arns created"
 }
 
 ## Create 3 IAM users with count ##
@@ -72,3 +72,21 @@ output "iam_names" {
   value       = [for user in aws_iam_user.terraform_user : user.name]
   description = "List of IAM user names created by terraform_user resource."
 }
+
+## Output for instances ##
+
+output "instance_public_ips" {
+  description = "Public IP addresses of the created EC2 instances."
+  value       = { for name, instance in aws_instance.instances : name => instance.public_ip }
+}
+
+# Output the IDs of the created instances
+output "instance_names" {
+  description = "IDs of the created EC2 instances."
+  value       = { for name, instance in aws_instance.instances : name => instance.id }
+}
+
+
+
+
+
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -6,33 +6,33 @@ variable "aws" {
 
   validation {
     error_message = "Value must be one of: dev, staging, prod"
-    condition       = contains(["dev", "staging", "prod"], var.aws)
+    condition     = contains(["dev", "staging", "prod"], var.aws)
   }
 }
 
 ## Create backup days variable ##
 
 variable "backup_days" {
-  type = number 
+  type    = number
   default = 7
 
   validation {
     error_message = "Backups should occur between 1 and 30 days"
-    condition = var.backup_days > 0 && var.backup_days <= 30
- }
+    condition     = var.backup_days > 0 && var.backup_days <= 30
+  }
 }
 
 ## Enable encryption variable ##
 
 variable "enable_encryption" {
-  type = bool
+  type    = bool
   default = false
 }
 
 ## Create availability zones variable ##
 
 variable "availability_zones" {
-  type = list(string)
+  type    = list(string)
   default = ["us-east-1a", "us-east-1b"]
 }
 
@@ -42,6 +42,47 @@ variable "instance_types" {
   type = map(string)
   default = {
     dev  = "t2.micro"
-    prod  = "t2.small"
+    prod = "t2.small"
   }
-} 
+}
+
+## Create 5 s3 buckets with names ##
+
+variable "named_instances" {
+  type = list(object({
+    name   = string
+    instance_type   = string
+    ami_id = string
+  }))
+
+  default = [{
+    name   = "Ruairi OConnell"
+    instance_type   = " t2.micro"
+    ami_id = "ami-000ec6c25978d5999"
+    },
+    {
+      name   = "Alan Hollowed"
+      instance_type   = "t2.micro"
+      ami_id = "ami-000ec6c25978d5999"
+    },
+    {
+      name   = "Aoife Kelly"
+      instance_type   = "t2.micro"
+      ami_id = "ami-000ec6c25978d5999"
+    },
+    {
+      name   = "Evgeny "
+      instance_type   = "t2.micro"
+      ami_id = "ami-000ec6c25978d5999"
+    },
+    {
+      name   = "Anton Sidelnekov"
+      instance_type   = "t2.micro"
+      ami_id = "ami-000ec6c25978d5999"
+  }]
+}
+
+variable "vpc_id" {
+  type = string
+  default = "vpc-0247ae9e1b35384bd"
+}
